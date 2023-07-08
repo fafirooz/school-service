6bb70eca115a6227a86d96d89a474b72e3ed0b2b
fafirooz <fa.firooz@gmail.com>
2023-07-08 15:50:57 -0700

    add  ApplicationTest

---
+++ src/test/java/ir/schoolservice/ApplicationTest.java
@@ -0,0 +1,11 @@
+package ir.schoolservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
